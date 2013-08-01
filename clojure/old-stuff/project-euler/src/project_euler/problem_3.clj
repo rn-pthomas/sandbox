382948f36b1538eb31fa2185d38bf7ec4b939aeb
pat-thomas <patthomassoftware@gmail.com>
2013-07-31 22:46:38 -0400

    begin working on project euler problem 3

--- clojure/old-stuff/project-euler/src/project_euler/problem_3.clj
+++ clojure/old-stuff/project-euler/src/project_euler/problem_3.clj
@@ -1,19 +1,21 @@
 (ns project-euler.project-3)
 
-(defn factors [num]
-  (filter (fn [x] (= 0 (mod num x))) (range 1 (inc num))))
-
-(defn primes [num]
-  (filter (fn [x] (= (count (factors x)) 2))
-          (concat [2] (range 1 (inc num) 2))))
-
 (defn prime? [num]
-  (= (count (factors num)) 2))
+  (loop [i 3]
+    (if (= (mod num i) 0)
+      false
+      (if (= i (- num 2))
+        true
+        (recur (+ i 2))))))
 
-(defn prime-factors [num]
-  (let [fs (factors num)]
-   (filter (fn [x] (and (prime? x)
-                       (some #(= % x) fs)))
-           fs)))
+(defn largest-factor [num]
+  (loop [i (dec num)]
+    (if (= (mod num i) 0)
+      i
+      (recur (dec i)))))
 
-(last (prime-factors 600851475143)) ;;
+(defn largest-prime-factor [num]
+  (loop [i (largest-factor num)]
+    (if (and (prime? i) (= #(mod num i) 0))
+      i
+      (recur (dec i)))))
