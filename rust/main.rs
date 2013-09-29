55b40f6f26cbaeb8e97586dba634b06f85830d36
pat-thomas <patthomassoftware@gmail.com>
2013-09-28 22:14:13 -0400

    rust, clojure, c stuff

--- rust/main.rs
+++ rust/main.rs
@@ -1,16 +1,12 @@
-/* This function creates ten "tasks" then executes them all concurrently. Run it
-   several times and observe the irregular linebreaks that result as each task
-	 races on stdout, due to the fact that `println` may yield to the task
-	 scheduler between printing its argument and printing a newline. */
 fn main() {
-	// `times` is a method on numeric types whose only argument is a closure.
-	for 10000.times {
-		do spawn {
-			// `each` is a method that accepts a closure with one argument.
-			for [1, 2, 3].each |item| {
-				// `fmt!` is a macro that statically verifies a format string.
-				println(fmt!("%d", *item));
-			}
-		}
-	}
+  let nums = [1,2];
+  let noms = ["Tim", "Eston", "Aaron", "Ben"];
+
+  let mut odds = nums.iter().map(|&x| x * 2 - 1);
+
+  for num in odds {
+    do spawn {
+      println!("{:s} says hello from a lightweight thread!", noms[num]);
+    }
+  }
 }
