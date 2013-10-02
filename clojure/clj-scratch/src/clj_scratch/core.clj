4155c5aa784d6fbf449f424b7711579dc1e06906
pat-thomas <patthomassoftware@gmail.com>
2013-10-02 09:00:34 -0400

    break scratch into scratch namespace

--- clojure/clj-scratch/src/clj_scratch/core.clj
+++ clojure/clj-scratch/src/clj_scratch/core.clj
@@ -1,23 +1,13 @@
 (ns clj-scratch.core
   (:use [quil.core]))
 
-(defn loop-state-until-done [fn-name wait initial-seed exit-pred]
-  (loop [i initial-seed]
+(defn loop-state-until-done
+  "Recursively calls a function until a predicate is met."
+  [fn-name wait initial-seed exit-pred]
+  (loop [i       initial-seed
+         counter 0]
     (if-not (exit-pred i)
-      (do (fn-name i)
-          (Thread/sleep wait)
-          (recur (inc i)))
-      i)))
-
-
-
-;;scratch...
-(defn side-effecty-function [n]
-  (println (format "doing stuff => %d" n)))
-
-(defn exit-pred [n]
-  (> n 10))
-
-(comment
-  (loop-state-until-done side-effecty-function 80 -20 exit-pred)
-  )
+      (do (Thread/sleep wait)
+          (recur (fn-name i) (inc counter)))
+      {:result  i
+       :counter counter})))
