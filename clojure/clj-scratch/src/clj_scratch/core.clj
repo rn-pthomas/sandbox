6dbcaa663d41b716b8f5abe62524ce24436b44f0
pat-thomas <patthomassoftware@gmail.com>
2013-10-03 22:00:50 -0400

    Begin implementing defmachine macro + comment

--- clojure/clj-scratch/src/clj_scratch/core.clj
+++ clojure/clj-scratch/src/clj_scratch/core.clj
@@ -1,5 +1,5 @@
 (ns clj-scratch.core
-  (:use [quil.core]))
+  (:require [quil.core :as quil]))
 
 (defn loop-until-done
   "Recursively calls a function until a predicate is met. All side effects should take place within the body of the function passed in."
@@ -12,3 +12,22 @@
                  (inc counter)))
       {:result  i
        :counter counter})))
+
+(defmacro defmachine
+  "Builds a simple state machine."
+  [name wait & body]
+  `(defn ~name []
+     "you still have to implement me!"))
+
+
+(comment
+
+  ;; NB> This is the code I want to write.
+  (defmachine some-cool-machine 100
+    (state some-state)
+    (state another-state)
+    (state the-last-state))
+
+  (some-cool-machine)
+
+  )
