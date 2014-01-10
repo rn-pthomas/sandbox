3dd137776a6693794cda2c11dee10bd64f0763d8
Pat Thomas <patthomassoftware@gmail.com>
2014-01-09 22:53:48 -0500

    some sequence utility functions

--- algorithm-sandbox/src/algorithm_sandbox/graph_traversal.clj
+++ algorithm-sandbox/src/algorithm_sandbox/graph_traversal.clj
@@ -1,5 +1,18 @@
 (ns algorithm-sandbox.graph-traversal)
 
+(defn drop-from-coll
+  ^{:doc "Given an index idx and a list l, drop the element at idx."}
+  [idx l]
+  (let [[start finish] (split-at idx l)]
+    (concat start (drop 1 finish))))
+
+(drop-from-coll 2 [1 2 3 4 5])
+
+(defn move-to-head
+  ^{:doc "Given an index idx and a list l, move the element at idx to the front of l."}
+  [idx l]
+  (cons (nth l idx) (drop-from-coll idx l)))
+
 (defn local-traversal [node-group]
   (let [[root-node other-nodes] node-group]
     (for [other-node other-nodes]
