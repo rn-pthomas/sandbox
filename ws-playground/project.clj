bb73fe11a188f8fd66a6cd04770ae9f72e0ea61b
Pat Thomas <patthomassoftware@gmail.com>
2014-01-11 11:20:52 -0500

    initial commit

---
+++ ws-playground/project.clj
@@ -0,0 +1,7 @@
+(defproject ws-playground "0.1.0-SNAPSHOT"
+  :description "experiments with web sockets"
+  :url "http://github.com/pat-thomas/sandbox"
+  :license {:name "Eclipse Public License"
+            :url   "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [http-kit            "2.1.16"]])
---
+++ ws-playground/src/ws_playground/core.clj
@@ -0,0 +1,6 @@
+(ns ws-playground.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
