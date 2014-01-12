45500cc397be42de8609ab07ed4d891b02e0a8ba
Pat Thomas <patthomassoftware@gmail.com>
2014-01-11 23:54:36 -0500

    parse request from json

--- ws-playground/src/ws_playground/socket.clj
+++ ws-playground/src/ws_playground/socket.clj
@@ -1,8 +1,11 @@
 (ns ws-sandbox.socket
-  (:require [org.httpkit.server :as http :refer [with-channel on-close on-receive send!]]))
+  (:require [org.httpkit.server :as http :refer [with-channel on-close on-receive send!]]
+            [clojure.data.json  :as json]))
 
 (defn handler [request]
   (with-channel request channel
-    (on-close   channel (fn [status] (println "channel closed: " status)))
+    (on-close   channel (fn [status] (println "channel closed:" status)))
     (on-receive channel (fn [data] ;; echo it back
-                               (send! channel data)))))
+                          (let [parsed-data (json/read-str data
+                                                           :key-fn keyword)]
+                            (send! channel data))))))
