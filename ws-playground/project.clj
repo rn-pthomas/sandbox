49cc25f0f770fab84d2b5b8656eaafa199119489
Pat Thomas <patthomassoftware@gmail.com>
2014-01-11 23:53:51 -0500

    clojure.data.json dependency

--- ws-playground/project.clj
+++ ws-playground/project.clj
@@ -1,8 +1,9 @@
 (defproject ws-playground "0.1.0-SNAPSHOT"
-  :description "experiments with web sockets"
-  :url         "http://github.com/pat-thomas/sandbox"
-  :license    {:name "Eclipse Public License"
-               :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http-kit            "2.1.16"]
-                 [compojure           "1.1.5"]])
+  :description  "experiments with web sockets"
+  :url          "http://github.com/pat-thomas/sandbox"
+  :license      {:name "Eclipse Public License"
+                 :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure   "1.5.1"]
+                 [http-kit              "2.1.16"]
+                 [compojure             "1.1.5"]
+                 [org.clojure/data.json "0.2.4"]])
