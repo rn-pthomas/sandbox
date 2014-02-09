7c01a7cd956369ddecb56e424bce93736b30a4a8
Pat Thomas <patthomassoftware@gmail.com>
2014-02-09 18:27:23 -0500

    Functions to render XML for Say and Gather.

--- clj-twilio-sandbox/src/clj_twilio_sandbox/twiml.clj
+++ clj-twilio-sandbox/src/clj_twilio_sandbox/twiml.clj
@@ -1,8 +1,12 @@
 (ns clj-twilio-sandbox.twiml
   (:require [clojure.data.xml :as xml]))
 
-(defn say [msg]
-  (xml/emit-str
-   (xml/element :Response {}
-                (xml/element :Say {:voice "woman"} msg))))
+(defn response [opts]
+  (xml/emit-str (xml/element :Response {} opts)))
 
+(defn say [msg]
+  (response (xml/element :Say {:voice "woman"} msg)))
+
+(defn gather [timeout finish-on-key say-msg]
+  (response (xml/element :Gather {:timeout timeout :finishOnKey finish-on-key}
+                         (xml/element :Say {:voice "woman"} say-msg))))
