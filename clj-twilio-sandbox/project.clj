9a50546ba06734694cc48fe986d0b3e0eb59c40b
Pat Thomas <patthomassoftware@gmail.com>
2014-02-09 18:05:45 -0500

    clojure.data.xml dependency.

--- clj-twilio-sandbox/project.clj
+++ clj-twilio-sandbox/project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main clj-twilio-sandbox.core
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http-kit            "2.1.16"]])
+  :dependencies [[org.clojure/clojure  "1.5.1"]
+                 [http-kit             "2.1.16"]
+                 [org.clojure/data.xml "0.0.7"]])
