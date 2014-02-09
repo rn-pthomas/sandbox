f4ef7fd1b6ae175f752dc0eaaf2d9feb36838962
Pat Thomas <patthomassoftware@gmail.com>
2014-02-09 17:55:49 -0500

    Remove old subproject.

--- ws-playground/src/ws_playground/dev.clj
+++
@@ -1,7 +0,0 @@
-(ns ws-playground.dev
-  (:require [clojure.tools.nrepl        :as nrepl]
-            [clojure.tools.nrepl.server :refer [start-server stop-server]]))
-
-(defonce nrepl-port 4000)
-
-(defonce repl-server (start-server :port nrepl-port))
--- ws-playground/test/ws_playground/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns ws-playground.core-test
-  (:require [clojure.test :refer :all]
-            [ws-playground.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
