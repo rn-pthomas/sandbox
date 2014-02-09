d2e15dbad98b900da03a101a6c4fc94873df5587
Pat Thomas <patthomassoftware@gmail.com>
2014-02-09 18:56:56 -0500

    Basic server.

--- clj-twilio-sandbox/src/clj_twilio_sandbox/core.clj
+++ clj-twilio-sandbox/src/clj_twilio_sandbox/core.clj
@@ -1,8 +1,18 @@
 (ns clj-twilio-sandbox.core
   (:require [clj-twilio-sandbox.twiml :as twiml]
-            [clj-twilio-sandbox.http  :as http]))
+            [clj-twilio-sandbox.http  :as http]
+            [org.httpkit.server       :as server]
+            [compojure.handler        :refer [site]]
+            [compojure.core           :refer [defroutes GET]]))
+
+(defn handle-twilio-get [req]
+  (println req))
+
+(defroutes app-routes
+  (GET "/twilio-app" [] handle-twilio-get))
 
 (defn -main [& args]
-  (println "i don't do anything yet..."))
+  (println "Booting server on port 8080...")
+  (server/run-server (site #'app-routes) {:port 8080}))
 
 
