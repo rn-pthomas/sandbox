57c901068415448e7c115533f0ad624bcd2c2441
Pat Thomas <pthomas@relaynetwork.com>
2014-02-14 11:43:45 -0500

    Whitespace.

--- ws-sandbox/project.clj
+++ ws-sandbox/project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "1.0.2"]]
   :cljsbuild {:builds [{:source-paths ["src/app"]
-                        :compiler {:output-to    "public/js/main.js"
-                                  :optimizations :whitespace
-                                  :pretty-print  true}}]}
+                        :compiler {:output-to     "public/js/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print  true}}]}
   :dependencies [[org.clojure/clojure       "1.5.1"]
                  [org.clojure/clojurescript "0.0-2156"]
                  [om                        "0.3.6"]
