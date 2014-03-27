6f6586fe15486efb3cc3ed46586fed6de1ee6358
Pat Thomas <pthomas@relaynetwork.com>
2014-03-26 22:56:32 -0400

    Some Haskell code that was sitting around.

--- haskell/x.hs
+++ haskell/x.hs
@@ -3,3 +3,27 @@ addThree x y z = x + y + z
 
 factorial :: Integer -> Integer
 factorial n = product [1..n]
+
+------------
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!!!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+myFactorial :: (Integral a) => a -> a
+myFactorial 0 = 1
+myFactorial n = n * myFactorial (n - 1)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+------------
+capital :: String -> String
+capital "" = "Empty string, whoops"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
