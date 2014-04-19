ed3222e3d9d719a36964ebd105f700b5136a62aa
Pat Thomas <pthomas@relaynetwork.com>
2014-04-18 20:00:52 -0400

    Namespacing = cleanliness.

--- clojure/cascalog-playground/src/cascalog_playground/core.clj
+++ clojure/cascalog-playground/src/cascalog_playground/core.clj
@@ -1,23 +1,11 @@
 (ns cascalog-playground.core
-  (:require [clojure.java.io     :as io]
-            [clojure.edn         :as edn]
-            [cascalog.api        :refer :all]
-            [cascalog.playground :refer :all]))
-
-(defn split-on-tab
-  [line]
-  (clojure.string/split line #"\t" 3))
+  (:require [cascalog-playground.file-utils :as file-utils]
+            [cascalog.playground            :as cascalog-playground]
+            [cascalog.api                   :refer :all]))
 
 (defn normalize-schema-definition
   [line]
   )
-
-(defn line-seq->events-seq
-  [s]
-  (map #(let [[event-type event-version event-schema] (map read-string (split-on-tab %))
-              event-type (keyword event-type)]
-          [event-type event-version event-schema])
-       s))
 
 (defn analyze-event
   [line accum]
@@ -25,42 +13,33 @@
         event-type (keyword event-type)]
     (assoc accum [event-type event-version] (conj (event-type accum) event-schema))))
 
-(defn get-event-counts
-  [analyzed-file]
-  (for [event-type (keys analyzed-file)]
-    [event-type (count (event-type analyzed-file))]))
-
 (defn analyze-event-file
   [filename]
   (->> filename
-       all-lines
-       line-seq->events-seq
+       file-utils/all-lines
+       file-utils/line-seq->events-seq
        (reduce (fn [acc [en ev es]]
                  (update-in acc [[en ev]] conj es))
                {})))
-
-(defn all-lines
-  [filename]
-  (with-open [rdr (io/reader filename)]
-    (doall (line-seq rdr))))
 
 (comment
 
   ;;counts distinct versions of schemas
   (let [fname "data/part-00000"]
     (->> fname
-         all-lines
-         line-seq->events-seq
+         file-utils/all-lines
+         file-utils/line-seq->events-seq
          (reduce (fn [acc [en ev es]]
                    (update-in acc [[en ev]] #(inc (or % 0))))
                  {})))
 
+  ;;keys [event-type event-version] to event-schema
   (let [fname "data/part-00000"]
     (->> fname
-         all-lines
-         line-seq->events-seq
+         file-utils/all-lines
+         file-utils/line-seq->events-seq
          (reduce (fn [acc [en ev es]]
                    (update-in acc [[en ev]] conj es))
                  {})))
-
+  
   )
