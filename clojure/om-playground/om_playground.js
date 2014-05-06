bf8f52b18f5a7b479ed56c3eadfa325a59f75c7e
Pat Thomas <pthomas@relaynetwork.com>
2014-05-06 13:03:22 -0400

    clear out om-playground project.

--- clojure/om-playground/om_playground.js
+++
@@ -1,5 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../om/dom.js", ['om.dom'], ['cljs.core']);
-goog.addDependency("../om/core.js", ['om.core'], ['cljs.core', 'om.dom', 'goog.ui.IdGenerator']);
-goog.addDependency("../om_playground/core.js", ['om_playground.core'], ['cljs.core', 'om.core', 'om.dom']);
