a99def2f2e51bef564dfc371a5e9e198fc15f0d8
Pat Thomas <patthomassoftware@gmail.com>
2014-06-07 18:53:28 -0400

    better loop

--- clojure/overtone-playground/src/overtone_playground/sounds.clj
+++ clojure/overtone-playground/src/overtone_playground/sounds.clj
@@ -9,12 +9,14 @@
      (line:kr 0 0.4 length FREE)))
 
 (comment
-  (loop [counter 0]
-    (when-not (> counter 10)
+  (def loop-running (atom true))
+
+  (loop []
+    (when-not @loop-running
       (let [res (json/read-json (core/echo "echo me!"))
             {:keys [a b c d e]} res]
-        (do (basic-saw (* 10.0 (:f e)) 0.5)
-            (recur (inc counter))))))
+        (do (basic-saw (* 100 (:f e)) 0.2)
+            (recur)))))
+
+  (reset! loop-running false)
  )
-
-
