57633451a8b18f2f5781b57d7c95609054642006
Pat Thomas <patthomassoftware@gmail.com>
2014-06-14 21:58:44 -0400

    quil dependency

--- clojure/overtone-playground/project.clj
+++ clojure/overtone-playground/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 ;;[http-kit            "2.1.16"]
+                 ;;[http-kit            "2.1.16"] ;; no support for websocket clients yet
                  [aleph               "0.3.2"]
                  [overtone            "0.9.1"]
-                 ])
+                 [quil                "2.1.0"]])
