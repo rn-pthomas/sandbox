8bb2caa16026d9e48c47e0ad90c7e496e9236d59
Pat Thomas <pthomas@relaynetwork.com>
2014-06-19 13:11:20 -0400

    some code from the web

--- clojure/markov-chains/src/markov_chains/core.clj
+++ clojure/markov-chains/src/markov_chains/core.clj
@@ -1,2 +1,39 @@
 (ns markov-chains.core)
 
+;; the following code was shamelessly ripped from:
+;; http://diegobasch.com/fun-with-markov-chains-and-clojure
+
+(defn markov-data
+  [text]
+  (let [maps
+        (for [line (clojure.string/split text #"\.")
+              m    (let [l     (str line ".")
+                         words (cons :start (clojure.string/split l #"\s+"))]
+                     (for [p (partition 2 1 (remove #(= "" %) words))]
+                       {(first p) [(second p)]}))]
+          m)]
+    (apply merge-with concat maps)))
+
+(defn sentence
+  [data]
+  (loop [ws  (data :start)
+         acc []]
+    (let [w    (rand-nth ws)
+          nws  (data w)
+          nacc (concat acc [w])]
+      (if (= \. (last w))
+        (clojure.string/join " " nacc)
+        (recur nws nacc)))))
+
+(comment
+  (->> (for [w ["He" "She" "It"]
+             x ["is" "was"]
+             y ["a" "the"]
+             z (map #(str % ".") ["girl" "boy" "cat" "crab"])]
+         (apply str (interpose " " [w x y z])))
+       (reduce (fn [acc word]
+                 (str acc word " "))
+               "")
+       markov-data
+       sentence)
+  )
