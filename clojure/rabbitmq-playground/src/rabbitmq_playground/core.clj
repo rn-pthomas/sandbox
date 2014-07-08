9242ea8fadddc9f3e822edbf582dfacc89981ae5
Pat Thomas <patthomassoftware@gmail.com>
2014-07-08 15:04:51 -0400

    Messages are passed as JSON.

--- clojure/rabbitmq-playground/src/rabbitmq_playground/core.clj
+++ clojure/rabbitmq-playground/src/rabbitmq_playground/core.clj
@@ -3,18 +3,20 @@
             [langohr.channel   :as channel]
             [langohr.queue     :as queue]
             [langohr.consumers :as consumers]
-            [langohr.basic     :as basic]))
+            [langohr.basic     :as basic]
+            [clojure.data.json :as json]))
 
 (def ^{:const true}
   default-exchange-name "")
 
 (defn message-handler
   [ch {:keys [content-type delivery-type delivery-tag type] :as meta} ^bytes payload]
-  (println (format "[consumer] Received a message: %s, delivery tag: %s, content type: %s, type: %s"
-                   (String. payload "UTF-8")
-                   delivery-tag
-                   content-type
-                   type)))
+  (let [payload (json/read-str (String. payload "UTF-8") :key-fn keyword)]
+    (println (format "[consumer] Received a message: %s, delivery tag: %s, content type: %s, type: %s"
+                     payload
+                     delivery-tag
+                     content-type
+                     type))))
 
 (defn initialize
   []
@@ -23,8 +25,7 @@
         qname "langohr.examples.hello-world"]
     (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
     (queue/declare ch qname :exclusive false :auto-delete true)
-    (consumers/subscribe ch qname message-handler :auto-ack true)
-    ))
+    (consumers/subscribe ch qname message-handler :auto-ack true)))
 
 (defn shutdown
   []
@@ -40,9 +41,9 @@
   (let [conn  (langohr/connect)
         ch    (channel/open conn)
         qname "langohr.examples.hello-world"]
-    (basic/publish ch default-exchange-name qname message :content-type "text/plain" :type "greetings.hi")))
+    (basic/publish ch default-exchange-name qname (json/json-str message) :content-type "application/json" :type "greetings.hi")))
 
 (comment
   (initialize)
-  (publish-message "hello")
+  (publish-message {:msg "hello"})
 )
