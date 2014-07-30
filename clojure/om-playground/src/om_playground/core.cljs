a4d1f06207c5da9f8098c76590cbf8e9807da11b
Pat Thomas <pthomas@relaynetwork.com>
2014-07-30 13:07:06 -0400

    More Om tutorial stuff.

--- clojure/om-playground/src/om_playground/core.cljs
+++ clojure/om-playground/src/om_playground/core.cljs
@@ -19,82 +19,71 @@
   [{:keys [first last] :as contact}]
   (str last ", " first (middle-name contact)))
 
-(defn parse-contact
-  [contact-str]
-  (let [[first middle last :as parts] (string/split contact-str #"\s+")
-        [first last middle]           (if (nil? last)
-                                        [first middle]
-                                        [first last middle])
-        middle                        (when middle (string/replace middle "." ""))
-        c                             (if middle
-                                        (count middle)
-                                        0)]
-    (when (>= (count parts) 2)
-      (cond-> {:first first :last last}
-              (== c 1) (assoc :middle-initial middle)
-              (>= c 2) (assoc :middle middle)))))
-
-(defn add-contact
-  [app owner]
-  (when-let [new-contact (-> (om/get-node owner "new-contact")
-                        .-value
-                        parse-contact)]
-    (om/transact! app :contacts #(conj % new-contact))
-    (om/set-state! owner :text "")))
-
-(defn contact-view
-  [contact owner]
+(defn student-view
+  [student owner]
   (reify
-    om/IRenderState
-    (render-state [this {:keys [delete]}]
+    om/IRender
+    (render [_]
+      (dom/li nil (display-name student)))))
+
+(defn professor-view
+  [professor owner]
+  (reify
+    om/IRender
+    (render [_]
       (dom/li nil
-              (dom/span nil (display-name contact))
-              (dom/button #js {:onClick (fn [e]
-                                          (async/put! delete @contact))}
-                          "Delete")))))
+              (dom/div nil (display-name professor))
+              (dom/label nil "Classes")
+              (apply dom/ul nil
+                     (map #(dom/li nil %) (:classes professor)))))))
 
-(defn handle-change
-  [e owner {:keys [text]}]
-  (let [value (.. e -target -value)]
-    (if-not (re-find #"[0-9]" value)
-      (om/set-state! owner :text value)
-      (om/set-state! owner :text text))))
+(defmulti entry-view
+  (fn [person _]
+    (:type person)))
 
-(defn render-root
+(defmethod entry-view :student
+  [person owner]
+  (student-view person owner))
+
+(defmethod entry-view :professor
+  [person owner]
+  (professor-view person owner))
+
+(defn people
+  [app]
+  (->> (:people app)
+       (mapv (fn [x]
+               (if (:classes x)
+                 (update-in x [:classes]
+                            (fn [cs]
+                              (mapv (:classes app) cs)))
+                 x)))))
+
+(defn registry-view
   [app owner]
   (reify
-    om/IInitState
-    (init-state [_]
-      {:delete (async/chan)
-       :text   ""})
-
-    om/IWillMount
-    (will-mount [_]
-      (let [delete (om/get-state owner :delete)]
-        (go (loop []
-              (let [contact (async/<! delete)]
-                (om/transact!
-                 app
-                 :contacts
-                 (fn [xs]
-                   (vec (remove #(= contact %) xs))))
-                (recur))))))
-
     om/IRenderState
-    (render-state [this state]
-      (dom/div nil
-               (dom/h2 nil "Contact list")
+    (render-state [_ state]
+      (dom/div #js {:id "registry"}
+               (dom/h2 nil "Registry")
                (apply dom/ul nil
-                      (om/build-all contact-view (:contacts app)
-                                    {:init-state state}))
-               (dom/div nil
-                        (dom/input
-                         #js {:type     "text"
-                              :ref      "new-contact"
-                              :value    (:text state)
-                              :onChange #(handle-change % owner state)})
-                        (dom/button #js {:onClick #(add-contact app owner)} "Add contact"))))))
+                      (om/build-all entry-view (people app)))))))
 
-(om/root render-root
+(defn classes-view
+  [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:id "classes"}
+               (dom/h2 nil "Classes")
+               (apply dom/ul nil
+                      (map #(dom/li nil %)
+                           (vals (:classes app))))))))
+
+(om/root registry-view
          state/app-state
-         {:target js/document.body})
+         {:target (. js/document (getElementById "registry"))})
+
+(om/root classes-view
+         state/app-state
+         {:target (. js/document (getElementById "classes"))})
