539bad220e36587aedcbb78ed6f822882ef2d9ef
Pat Thomas <patthomassoftware@gmail.com>
2014-09-10 19:34:24 -0400

    Break out search-results-list fn to be more generic.

--- clojure/spotify-client-playground/src/spotify_client_playground/app/core.cljs
+++ clojure/spotify-client-playground/src/spotify_client_playground/app/core.cljs
@@ -21,24 +21,31 @@
     (xhr/do-xhr {:method      :get
                  :url         search-url
                  :on-complete (fn [resp]
-                                (let [parsed-resp (parse-json-resp resp)
-                                      items       (get-in parsed-resp ["result-set" "artists" "items"])
+                                (let [parsed-resp  (parse-json-resp resp)
+                                      items        (get-in parsed-resp ["result-set" "artists" "items"])
                                       artist-names (->> items
                                                         (mapv (fn [item]
                                                                 (get item "name")))
                                                         sort)]
-                                  (swap! app-state assoc-in [:search :results] artist-names)))})))
+                                  (swap! app-state assoc-in [:search :results] (get parsed-resp "result-set"))
+                                  ;;(swap! app-state assoc-in [:search :results] artist-names)
+                                  ))})))
 
 (defn search-results-list
-  [search-results]
-  (apply dom/ul
-         #js {:id "result-list"}
-         (mapv (fn [res]
-                 (dom/li #js {:onClick   (fn [e]
-                                           (println "clicked!"))
-                              :className "result-list-item"}
-                         res))
-               search-results)))
+  [search-results path key-from-item]
+  (let [list-to-render (if key-from-item
+                         (mapv (fn [item]
+                                 (get item key-from-item))
+                               (get-in search-results path))
+                         (get-in search-results path))]
+    (apply dom/ul
+          #js {:id "result-list"}
+          (mapv (fn [res]
+                  (dom/li #js {:onClick   (fn [e]
+                                            (println "clicked!"))
+                               :className "result-list-item"}
+                          res))
+                list-to-render))))
 
 (defn main-app
   [app owner opts]
@@ -56,7 +63,7 @@
                                              (spotify-search-xhr search-term-val)))}
                            "Search")
                (when-let [search-results (get-in app [:search :results])]
-                 (search-results-list search-results))))))
+                 (search-results-list search-results ["artists" "items"] "name"))))))
 
 (let [target (gdom/getElement "app-container")]
   (om/root main-app
