f7ce8f7bdd6cb794eab6f156f76e496032f6469b
Pat Thomas <pthomas@relaynetwork.com>
2014-09-11 15:38:32 -0400

    Filter list kinda works now.

--- clojure/spotify-client-playground/src/spotify_client_playground/app/core.cljs
+++ clojure/spotify-client-playground/src/spotify_client_playground/app/core.cljs
@@ -8,7 +8,7 @@
 
 (enable-console-print!)
 
-(def app-state (atom {}))
+(def app-state (atom {:search {:type "artists"}}))
 
 (defn spotify-search-xhr
   [search-term]
@@ -19,23 +19,65 @@
                                 (let [parsed-resp (parse-json-resp resp)]
                                   (swap! app-state assoc-in [:search :results] (get parsed-resp "result-set"))))})))
 
+(defn handle-change
+  [e data edit-key owner]
+  (om/transact! data edit-key (fn [_]
+                                (.. e -target -value))))
+
+(defn search-filter-dropdown
+  [data owner opts]
+  (reify
+    om/IDisplayName
+    (display-name [_]
+      (or (:react-name opts) "Search Filter Dropdown"))
+    
+    om/IRenderState
+    (render-state [_ opts]
+      (dom/h1 "Select search type")
+      (dom/select #js {:id          "search-filter-dropdown"
+                       :placeholder "artists"
+                       :onChange    (fn [e]
+                                      (handle-change e data :type owner))}
+                  (dom/option nil "artists")
+                  (dom/option nil "tracks")
+                  (dom/option nil "albums")))))
+
+(defn search-results-list-item
+  [search-result]
+  (dom/li #js {:onClick   (fn [e]
+                            (println "clicked!"))
+               :className "result-list-item"}
+          search-result))
+
+(defn some-component
+  [data owner opts]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (println "mounted"))
+    
+    om/IRenderState
+    (render-state [_ opts]
+      (dom/div nil "hey there"))))
+
 (defn search-results-list
-  [search-results search-type key-from-item]
-  (let [correct-path      [search-type search-type "items"]
-        results-to-render (get-in search-results correct-path)
-        list-to-render    (if key-from-item
-                            (mapv (fn [item]
-                                    (get item key-from-item))
-                                  results-to-render)
-                            results-to-render)]
-    (apply dom/ul
-          #js {:id "result-list"}
-          (mapv (fn [res]
-                  (dom/li #js {:onClick   (fn [e]
-                                            (println "clicked!"))
-                               :className "result-list-item"}
-                          res))
-                list-to-render))))
+  [data owner opts]
+  (reify
+    om/IDisplayName
+    (display-name [_]
+      (or (:react-name opts) "Search Results List"))
+    
+    om/IRenderState
+    (render-state [_ opts]
+      (let [search-results (:results data)
+            search-type    (:type data)
+            list-to-render (mapv #(get % "name")
+                                 (get-in search-results [search-type search-type "items"]))]
+        (dom/div nil
+                 (apply dom/ul
+                        #js {:id "result-list"}
+                        (mapv search-results-list-item list-to-render))
+                 (om/build search-filter-dropdown data))))))
 
 (defn main-app
   [app owner opts]
@@ -43,6 +85,14 @@
     om/IDisplayName
     (display-name [_]
       (or (:react-name opts) "App"))
+
+    om/IWillMount
+    (will-mount [_]
+      (xhr/do-xhr {:method      :get
+                   :url         "ping"
+                   :on-complete (fn [resp]
+                                  (when (= resp "ping")
+                                    (println "server is up and running")))}))
 
     om/IRender
     (render [this]
@@ -52,11 +102,15 @@
                                            (let [search-term-val (.-value (.getElementById js/document "search-input"))]
                                              (spotify-search-xhr search-term-val)))}
                            "Search")
-               (when-let [search-results (get-in app [:search :results])]
-                 (search-results-list search-results "artists" "name"))))))
+               (when (get-in app [:search :results])
+                 (om/build search-results-list (:search app)))))))
 
-(let [target (gdom/getElement "app-container")]
-  (om/root main-app
-           app-state
-           {:target target
-            :opts   {}}))
+(defn initialize-app!
+  []
+  (let [target (gdom/getElement "app-container")]
+    (om/root main-app
+             app-state
+             {:target target
+              :opts   {}})))
+
+(initialize-app!)
