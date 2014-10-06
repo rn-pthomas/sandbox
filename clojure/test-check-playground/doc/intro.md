e26912259308d0d5613e5fec00466bddb816e4e8
Pat Thomas <pthomas@relaynetwork.com>
2014-10-06 11:22:36 -0400

    Init repo.

---
+++ clojure/test-check-playground/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to test-check-playground
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/test-check-playground/test/test_check_playground/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-check-playground.core-test
+  (:require [clojure.test :refer :all]
+            [test-check-playground.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
