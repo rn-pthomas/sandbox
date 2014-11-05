0ce25af730abc6b1a8ca315769692745c2f84923
Pat Thomas <patthomassoftware@gmail.com>
2014-11-05 18:52:12 -0500

    hey, some core typed stuff

--- clojure/core-typed-playground/src/core_typed_playground/core.clj
+++ clojure/core-typed-playground/src/core_typed_playground/core.clj
@@ -1,13 +1,25 @@
 (ns core-typed-playground.core
-  (:require [clojure.core.typed :refer [ann check-ns]]))
+  (:refer-clojure :exclude [defn])
+  (:require [clojure.core.typed :as t :refer [ann cf check-ns defalias defn]]))
 
-(defn add-two
-  [number]
-  (+ number 2))
+(defmacro defandreturn
+  [thing]
+  `(do (def ~thing ~thing)
+       ~thing))
 
-(ann add-two [Number -> Number])
+;; always type check on compile
+(let [make-readable-type-error (fn [ex]
+                                 (let [errors (->> ex .getData :errors (map #(.getData %)))
+                                       friendly-errors (->> errors (map (fn [error]
+                                                                          (format "Form: %s, Found error at line %s, column %s"
+                                                                                  (-> error :form)
+                                                                                  (-> error :env :line)
+                                                                                  (-> error :env :column)))))]
+                                   (defandreturn friendly-errors)))
+      friendly-check-ns        (fn []
+                                 (try (check-ns)
+                                      (catch Exception e
+                                        (make-readable-type-error e))
+                                      (finally :ok)))]
+  (friendly-check-ns))
 
-(comment
-  (check-ns)
-  (add-two 1)
-  )
