1749d7ff29b1f81c6fc9e9c031255d7ec45fd179
Pat Thomas <patthomassoftware@gmail.com>
2014-11-05 20:43:11 -0500

    well, that was fun to write.

---
+++ clojure/clj-playground/src/clj_playground/http.clj
@@ -0,0 +1,60 @@
+(ns clj-playground.http
+  (:require [org.httpkit.client :as http]
+            [clojure.data.json  :as json]
+            [clojure.java.io    :refer :all]))
+
+(def base-api-url "https://api.discogs.com/")
+
+(defn parse-json-resp
+  [resp]
+  (-> resp deref :body (json/read-str :key-fn keyword)))
+
+(defn get-all-pages
+  [folder-resp]
+  (loop [accum        []
+         current-resp folder-resp]
+    (if-let [next-page-url (-> current-resp :pagination :urls :next)]
+      (recur (conj accum current-resp) (-> next-page-url http/get parse-json-resp))
+      (conj accum current-resp))))
+
+(defn folder-resp->releases
+  [folder-resp]
+  (let [raw-releases (->> folder-resp get-all-pages (map :releases) (mapcat #(map :basic_information %)))]
+    (map (fn [release]
+           (-> release
+               (select-keys [:title :artists :year])
+               (update-in [:artists] (fn [artist-rec]
+                                       (->> artist-rec first :name)))
+               (clojure.set/rename-keys {:artists :artist})))
+         raw-releases)))
+
+(defn get-user
+  [username]
+  (let [request-url (str base-api-url "users/" username "/collection/folders")
+        folder-urls (->> request-url http/get parse-json-resp :folders (map :resource_url))]
+    (->> folder-urls
+         (mapv (fn [folder-url]
+                     (-> folder-url
+                         (str "/releases")
+                         http/get
+                         parse-json-resp
+                         folder-resp->releases)))
+         flatten
+         (sort-by :year)
+         (mapv (fn fix-year [release]
+                 (update-in release [:year] #(if (= % 0)
+                                               "Unknown year"
+                                               %))))
+         (mapv (fn format-release [release]
+                 (format "[%s] %s - %s"
+                         (:year   release)
+                         (:artist release)
+                         (:title  release)))))))
+
+(comment
+  (def my-collection (get-user "OatRhombus"))
+  (with-open [wrtr (writer "./shuffled_collection.txt")]
+    (doseq [line (shuffle my-collection)]
+      (.write wrtr (str line "\n"))))
+  )
+
