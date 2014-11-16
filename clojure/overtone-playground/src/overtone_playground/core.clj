11cbdc3ab79024bcaa05497afb19c972a4234cef
Pat Thomas <patthomassoftware@gmail.com>
2014-11-16 12:44:48 -0500

    clean out old project

---
+++ clojure/overtone-playground/src/overtone_playground/core.clj
@@ -0,0 +1,10 @@
+(ns overtone-playground.core
+  (:require [clj-utils.lang-utils :refer :all]
+            [overtone.live        :refer :all]))
+
+
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
