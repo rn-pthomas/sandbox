bcbeb34c6314af97f4b515ef661c0445f8c649f6
Pat Thomas <pthomas@relaynetwork.com>
2014-12-18 10:22:33 -0500

    om-playground cljsbuild config.

---
+++ om-playground/out/om_playground/core.js
@@ -0,0 +1,11 @@
+// Compiled by ClojureScript 0.0-2496
+goog.provide('om_playground.core');
+goog.require('cljs.core');
+/**
+* I don't do a whole lot.
+*/
+om_playground.core.foo = (function foo(x){
+return cljs.core.println.call(null,x,"Hello, World!");
+});
+
+//# sourceMappingURL=core.js.map?rel=1418913487113
--- om-playground/project.clj
+++ om-playground/project.clj
@@ -3,4 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure       "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2496"]
+                 [om                        "0.8.0-beta3"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :source-paths ["src"]
+  :cljsbuild {:builds       [{:id "om-playground"
+                              :source-paths ["src"]
+                              :compiler     {:output-to     "om_playground.js"
+                                             :output-dir    "out"
+                                             :optimizations :none
+                                             :source-map    true}}]})
