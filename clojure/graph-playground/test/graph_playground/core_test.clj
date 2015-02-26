12ad06ced09c2d68f4e5b5ffc33622088adcbccf
Pat Thomas <pthomas@relaynetwork.com>
2015-02-25 22:15:34 -0500

    initial commit: graph-playground

---
+++ clojure/graph-playground/test/graph_playground/core_test.clj
@@ -0,0 +1,7 @@
+(ns graph-playground.core-test
+  (:require [clojure.test :refer :all]
+            [graph-playground.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
