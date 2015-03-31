ac22895b2dd7ae12f3c70268ddf27d35d2860214
Pat Thomas <pthomas@relaynetwork.com>
2015-03-31 00:09:36 -0400

    Messing around with overtone

--- clojure/core-async-playground/src/core_async_playground/core.clj
+++ clojure/core-async-playground/src/core_async_playground/core.clj
@@ -1,19 +1,41 @@
 (ns core-async-playground.core
-  (:require [clojure.core.async :as async :refer [go chan <! >!]]))
+  (:require [clojure.core.async :as async :refer [go chan <! >! alts! timeout]]
+            [core-async-playground.sounds :as sounds]))
 
-(def test-chan (chan))
+(defn seed-values
+  [timeout-value & words]
+  (let [acc (chan)]
+    (doseq [word words]
+      (go
+        (>! acc word)
+        (<! (timeout (rand-int timeout-value)))))
+    acc))
 
-(defn respond-to-event
-  [evt]
-  (println {:evt evt}))
+(defn handle-sound
+  [sound]
+  (condp = sound
+    "kick" (sounds/kick)
+    "snare" (sounds/snare)
+    "hat" (sounds/snap)
+    "tom" (sounds/tom)))
 
-(defn do-stuff
-  []
-  (go (while true
-        (respond-to-event (<! test-chan)))))
+(defn main
+  [wait]
+  (let [evts           (apply (partial seed-values 1000) (->> ["kick" "snare" "hat" "tom"] (repeat 12) flatten))
+        possible-waits [(/ wait 2.0) (/ wait 3.0)]]
+    (go
+      (while true
+        (when-let [evt (<! evts)]
+          (do
+            (println evt)
+            (handle-sound evt)))
+        (<! (timeout wait))))
+    (go
+      (while true
+        (when-let [evt (<! evts)]
+          (println (format "        %s" evt))
+          (handle-sound evt))
+        (<! (timeout (rand-nth possible-waits))))))
+  :running)
 
-(comment
-  (do-stuff)
-  (async/put! test-chan 10)
-  (async/put! test-chan 12)
-  )
+(main 700)
---
+++ clojure/core-async-playground/src/core_async_playground/sounds.clj
@@ -0,0 +1,7 @@
+(ns core-async-playground.sounds
+  (:require [overtone.live :as o]))
+
+(def snare (o/freesound 26903))
+(def kick (o/freesound 777))
+(def snap (o/freesound 87731))
+(def tom (o/freesound 25649))
