7361855dc765cf41c8b157a705490843617ebd33
Pat Thomas <pthomas@relaynetwork.com>
2015-04-08 14:17:41 -0400

    Oops.

--- clojure/transition-cljs/project.clj
+++ clojure/transition-cljs/project.clj
@@ -1,15 +1,15 @@
-3(defproject transition-cljs "0.1.0-SNAPSHOT"
-   :description "FIXME: write description"
-   :url "http://example.com/FIXME"
-   :license {:name "Eclipse Public License"
-             :url "http://www.eclipse.org/legal/epl-v10.html"}
-   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
-   :cljsbuild {:builds [{:source-paths ["src"]
-                         :compiler     {:output-to     "transition_cljs.js"
-                                        :output-dir    "out"
-                                        :optimizations :none
-                                        :source-map    true
-                                        ;;:pretty-print  true
-                                        }}]}
-   :dependencies [[org.clojure/clojure       "1.6.0"]
-                  [org.clojure/clojurescript "0.0-3126"]])
+(defproject transition-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler     {:output-to     "transition_cljs.js"
+                                       :output-dir    "out"
+                                       :optimizations :none
+                                       :source-map    true
+                                       ;;:pretty-print  true
+                                       }}]}
+  :dependencies [[org.clojure/clojure       "1.6.0"]
+                 [org.clojure/clojurescript "0.0-3126"]])
