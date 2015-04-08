6b6e6e870b40c743f818e169f257ef17a11d9f55
Pat Thomas <pthomas@relaynetwork.com>
2015-04-08 14:14:59 -0400

    Init new project

---
+++ clojure/transition-cljs/test/transition_cljs/core_test.clj
@@ -0,0 +1,7 @@
+(ns transition-cljs.core-test
+  (:require [clojure.test :refer :all]
+            [transition-cljs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
