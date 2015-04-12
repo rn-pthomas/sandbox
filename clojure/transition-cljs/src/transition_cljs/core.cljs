4fee5e53c1b919be0175527a3c9ebd36a2b8ced8
Pat Thomas <pthomas@relaynetwork.com>
2015-04-12 09:07:07 -0400

    Remove cruft.

--- clojure/transition-cljs/src/transition_cljs/core.cljs
+++ clojure/transition-cljs/src/transition_cljs/core.cljs
@@ -1,6 +1,5 @@
 (ns transition-cljs.core
-  (:require [transition-cljs.components.navbar :as navbar]
-            [transition-cljs.test              :as test]
+  (:require [transition-cljs.components.navbar :as navbar]  
             [transition-cljs.loader            :as loader]
             [om.core                           :as om]
             [om.dom                            :as dom])
@@ -11,29 +10,19 @@
 (def app-state
   (atom {}))
 
-(defcomponent debugger
-  (render
-   (apply dom/div
-          nil
-          (map (fn [{:keys [name content]}]
-                 (dom/div nil (str name " => " content)))
-               (sort (:test-suite data))))))
-
 (defcomponent app
   (render
    (dom/div
     nil
-    ;;(om/build debugger data)
-    (om/build navbar/navbar data)
-    )))
+    (om/build navbar/navbar data))))
 
 (defn main
   []
-  ;;(swap! app-state assoc :test-suite (util/load-suite))
   (loader/load-suite app-state)
   (om/root
    app
    app-state
    {:target (. js/document (getElementById "app-container"))}))
+
 
 (main)
