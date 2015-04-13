e1c7aadde2690b59288757ed529f4bac144621d0
Pat Thomas <pthomas@relaynetwork.com>
2015-04-13 09:40:12 -0400

    More fleshed out example.

--- clojure/transition-cljs/examples/tests/example_two.cljs
+++ clojure/transition-cljs/examples/tests/example_two.cljs
@@ -1,4 +1,11 @@
 (deftest example-two
-  (do
-    (println "Foof")
-    (println "Doof")))
+  (fixtures
+   {:consumer {:first-name "Pat"
+               :last-name  "Thomas"}})
+  (helpers
+   (assert-session-status
+    []
+    :do-something-to-assert-session-status))
+  (states
+   (init (to do-something assert-session-status)
+         (to fail-immediately bad-session))))
