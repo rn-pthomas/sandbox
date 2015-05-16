96b1b533748a34851d4c7ca389a7246b1a5f87b0
Pat Thomas <patthomassoftware@gmail.com>
2015-05-16 12:53:05 -0400

    figwheel build options

--- clojure/om-playground/project.clj
+++ clojure/om-playground/project.clj
@@ -2,20 +2,22 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure       "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
+  :dependencies [[org.clojure/clojure       "1.7.0-beta2"]
+                 [org.clojure/clojurescript "0.0-3269"]
                  [org.clojure/core.async    "0.1.267.0-0d7780-alpha"]
-                 [om                        "0.6.5"]
-                 [om-sync                   "0.1.1"]
+                 [org.omcljs/om             "0.8.8"]
                  [sablono                   "0.3.4"]
                  [om-utils                  "0.4.0"]]
-  :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.2.9"]]
+  :plugins [[lein-cljsbuild "1.0.6"]
+            [lein-figwheel "0.3.1"]]
   :source-paths ["src"]
-  :cljsbuild {:builds       [{:id           "om-playground"
-                              :source-paths ["src"]
-                              :figwheel {:on-jsload "om-playground.core/reload-hook"}
-                              :compiler     {:output-to     "om_playground.js"
-                                             :output-dir    "out"
-                                             :optimizations :none
-                                             :source-map    true}}]})
+  :cljsbuild {:builds [{:id           "om-playground"
+                        :source-paths ["src"]
+                        :figwheel     {:on-jsload "om-playground.core/reload-hook"}
+                        :compiler     {:asset-path    "js/out"
+                                       :output-to     "resources/public/js/om_playground.js"
+                                       :output-dir    "resources/public/js/out"
+                                       :optimizations :none
+                                       :source-map    true}}]}
+  :figwheel {:http-server-root "public"
+             :css-dirs         ["resources/public/css"]})
