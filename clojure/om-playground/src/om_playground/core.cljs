f406380b7fdc8b68cea8884c027210aebb9fd63a
Pat Thomas <pthomas@relaynetwork.com>
2015-05-17 21:31:51 -0400

    clear things out

--- clojure/om-playground/src/om_playground/core.cljs
+++ clojure/om-playground/src/om_playground/core.cljs
@@ -8,7 +8,7 @@
             [om-playground.animation        :as anim]
             [cljs.core.async                :as async :refer [chan timeout <! >!]]
             [om-playground.controllers.core           :refer [notify]])
-  (:require-macros [om-utils.core :refer [defcomponent]]
+  (:require-macros [om-utils.core          :refer [defcomponent]]
                    [cljs.core.async.macros :refer [go]]))
 
 (enable-console-print!)
@@ -17,34 +17,9 @@
   [& files]
   (println files))
 
-(defn build-cell
-  [data opts]
-  (om/build cmp/cell data {:opts opts}))
-
-(defn build-cell-grid
-  [{:keys [width height data cell-opts] :as params}]
-  (apply
-   (partial dom/div nil)
-   (for [y (range height)]
-     (apply
-      (partial dom/div nil)
-      (for [x (range width)]
-        (build-cell data (assoc cell-opts :x x :y y)))))))
-
-(def grid
-  {:height 28
-   :width  28})
-
 (defcomponent app
   (render
-   (apply
-    (partial dom/div nil)
-    (for [y (range 16)]
-      (apply
-       (partial dom/div nil)
-       (for [x (range 8)]
-         (om/build cmp/plain-cell data {:opts {:x x
-                                               :y y}})))))))
+   (dom/div nil "foof")))
 
 (om/root
  app
