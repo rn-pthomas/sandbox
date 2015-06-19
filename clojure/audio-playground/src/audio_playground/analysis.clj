227940db917b9cb16291d805b2d4b881faa08538
Pat Thomas <pthomas@relaynetwork.com>
2015-06-19 09:52:51 -0400

    more comments

--- clojure/audio-playground/src/audio_playground/analysis.clj
+++ clojure/audio-playground/src/audio_playground/analysis.clj
@@ -3,4 +3,5 @@
 
 (comment
   (playback/play (playback/file->clip "/Users/pthomas/Downloads/ticket_to_ride_rev.wav"))
+  (playback/play-loop (playback/file->clip "/Users/pthomas/Downloads/ticket_to_ride_rev.wav"))
   )
