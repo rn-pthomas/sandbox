232149874f93b466b32a070fb4f65a6cc8590659
Pat Thomas <patthomassoftware@gmail.com>
2015-06-25 23:22:01 -0400

    never mind

--- clojure/lookml/src/lookml/core.clj
+++
@@ -1,6 +0,0 @@
-(ns lookml.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- clojure/lookml/test/lookml/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns lookml.core-test
-  (:require [clojure.test :refer :all]
-            [lookml.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
