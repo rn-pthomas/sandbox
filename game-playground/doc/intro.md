1912859c70512e19c64c18b30b45ded3433b7da9
Pat Thomas <patthomassoftware@gmail.com>
2015-09-01 22:49:33 -0400

    new lein project

---
+++ game-playground/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to game-playground
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ game-playground/test/game_playground/core_test.clj
@@ -0,0 +1,7 @@
+(ns game-playground.core-test
+  (:require [clojure.test :refer :all]
+            [game-playground.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
