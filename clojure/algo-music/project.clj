c72f6f42bc87fc78e703673397642187d8ef811a
Pat Thomas <patthomassoftware@gmail.com>
2015-09-06 18:48:53 -0400

    core server ns

--- clojure/algo-music/project.clj
+++ clojure/algo-music/project.clj
@@ -4,10 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure       "1.7.0"]
+                 [org.clojure/core.typed    "0.3.11"]
                  [org.clojure/clojurescript "1.7.122"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [reagent "0.5.0"]]
+                 [org.clojure/core.async    "0.1.346.0-17112a-alpha"]
+                 [reagent                   "0.5.0"]]
 
   :plugins [[lein-cljsbuild "1.1.0"]
             [lein-figwheel "0.3.9"]]
