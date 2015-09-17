26787a010e6f17dfa201f09ad85fbf2e1263033d
Pat Thomas <pthomas@relaynetwork.com>
2015-09-17 10:58:31 -0400

    Basic rendering.

--- clojure/reagent-playground/src/reagent_playground/core.cljs
+++ clojure/reagent-playground/src/reagent_playground/core.cljs
@@ -6,20 +6,22 @@
 
 (println "Edits to this text should show up in your developer console.")
 
-;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:text "Hello world!!!"
+                          :size 8}))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn app
+  []
+  (let [size (:size @app-state)
+        rows (for [_ (range size)]
+               [:div.box])]
+    [:div (for [_ (range size)]
+            [:div.column rows])]))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
-
-(reagent/render-component [hello-world]
-                          (. js/document (getElementById "app")))
+(reagent/render-component
+ [app]
+ (. js/document (getElementById "app")))
 
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
-
+(defn on-js-reload
+  [& files]
+  (println files))
