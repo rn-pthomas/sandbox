c906bdbba0f19a0d0ea909f7493d34d108844dd5
Pat Thomas <pthomas@relaynetwork.com>
2015-09-20 11:18:34 -0400

    More animation stuff.

--- clojure/reagent-playground/src/reagent_playground/core.cljs
+++ clojure/reagent-playground/src/reagent_playground/core.cljs
@@ -1,27 +1,53 @@
-(ns ^:figwheel-always reagent-playground.core
-    (:require
-              [reagent.core :as reagent :refer [atom]]))
+(ns ^:figwheel-always
+  reagent-playground.core
+  (:require [reagent.core    :as reagent :refer [atom]]
+            [cljs.core.async :as async])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (enable-console-print!)
-
-(println "Edits to this text should show up in your developer console.")
-
-(defonce app-state (atom {:text "Hello world!!!"
-                          :size 8}))
-
-(defn app
-  []
-  (let [size (:size @app-state)
-        rows (for [_ (range size)]
-               [:div.box])]
-    [:div (for [_ (range size)]
-            [:div.column rows])]))
-
-(reagent/render-component
- [app]
- (. js/document (getElementById "app")))
-
 
 (defn on-js-reload
   [& files]
   (println files))
+
+(defonce app-state (atom {:text "Hello world!!!"
+                          :size 8
+                          :loop-running false}))
+
+(defn animation-loop
+  []
+  (let [anim-ch (async/timeout 500)]
+    (swap! app-state (assoc :loop-running true))
+    (go-loop []
+      (async/<! anim-ch)
+      (println "hey there...")
+      (let [size (:size @app-state)
+            highlighted [(rand-int size) (rand-int size)]]
+        (swap! app-state assoc-in [:highlighted] highlighted))
+      (recur))))
+
+(defn debugger
+  []
+  [:div
+   [:p (str "highlighted: " (:highlighted @app-state))]])
+
+(defn app
+  []
+  [:div
+   (let [size (:size @app-state)
+         rows (for [x (range size)
+                    y (range size)]
+                [:div.column
+                 [:div.box]])])
+   (debugger)])
+
+(defn main
+  []
+  (do
+    (when-not (:loop-running @app-state)
+      (animation-loop))
+    (reagent/render-component
+     [app]
+     (. js/document (getElementById "app")))))
+
+(main)
