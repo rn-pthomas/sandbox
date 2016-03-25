892403295706dc95e6b993934f9c1fa993f16183
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-03-25 14:12:12 -0400

    almost done

--- ss/main.go
+++ ss/main.go
@@ -4,18 +4,18 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	//"io"
-	//"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"net/http"
 	"strconv"
-	//"strings"
 )
 
+type Coordinate [2] float64
+type Polygon []Coordinate
 type StateData struct {
 	State string
-	Border [][2]float32
+	Border Polygon
 }
 
 func readStates() []StateData {
@@ -41,21 +41,63 @@ func readStates() []StateData {
 	return states
 }
 
-func handleRequest(r *http.Request) string {
+func insideBoundaries(x float64, y float64, poly Polygon) bool {
+	n := len(poly)
+	inside := false
+	var xints float64
+	
+	p1x := poly[0][0]
+	p1y := poly[0][1]
+
+	for i := 0; i <= n; i++ {
+		p2x := poly[i % n][0]
+		p2y := poly[i % n][1]
+		if y > math.Min(p1y, p2y) {
+			if y <= math.Max(p1y, p2y) {
+				if x <= math.Max(p1x, p2x) {
+					if p1y != p2y {
+						xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+					}
+					if p1x == p2x || x <= xints {
+						inside = !inside
+					}
+				}
+			}
+		}
+		p1x = p2x
+		p1y = p2y
+	}
+	
+	return inside
+}
+
+func findMatchingState(latitude float64, longitude float64, states []StateData) string {
+	var match string
+	for i := 0; i < len(states); i++ {
+		state := states[i]
+		fmt.Printf("testing state %s\n", state.State)
+		if insideBoundaries(latitude, longitude, state.Border) {
+			match = state.State
+			break
+		}
+	}
+	return match
+}
+
+func handleRequest(r *http.Request, states []StateData) string {
 	r.ParseForm()
 	latitude, _ := strconv.ParseFloat(r.FormValue("latitude"), 64)
 	longitude, _ := strconv.ParseFloat(r.FormValue("longitude"), 64)
-	fmt.Printf("latitude => %q\n", latitude)
-	fmt.Printf("longitude => %q\n", longitude)
-	return "pong!"
+
+	return findMatchingState(latitude, longitude, states)
 }
 
 func main() {
-	//states := readStates()
+	states := readStates()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "%q", handleRequest(r))
+		fmt.Fprintf(w, "%q", handleRequest(r, states))
 	})
-
+	fmt.Println("server running on port 8080...\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
