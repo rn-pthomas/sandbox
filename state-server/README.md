83776e5333213bb83c3b5ec61626c4a1657f79f2
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-03-30 14:31:38 -0400

    new scala project

---
+++ state-server/README.md
@@ -0,0 +1,12 @@
+# State Server #
+
+## Build & Run ##
+
+```sh
+$ cd State_Server
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ state-server/src/main/scala/com/ptsoftware/stateserver/MyScalatraServlet.scala
@@ -0,0 +1,16 @@
+package com.ptsoftware.stateserver
+
+import org.scalatra._
+
+class MyScalatraServlet extends StateServerStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
