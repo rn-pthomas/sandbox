88d7d288a97846ec31447c8dd82ed0a06a126ffa
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-04-26 19:02:24 -0400

    playing around with stuff

--- file-validation/src/file_validation/core.clj
+++ file-validation/src/file_validation/core.clj
@@ -1,6 +1,35 @@
-(ns file-validation.core)
+(ns file-validation.core
+  (:require [iota                  :as iota]
+            [clojure.string        :as string]
+            [clojure.core.reducers :as reducers]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn split-line
+  [line]
+  (string/split line #"\t"))
+
+(defn remove-header
+  [lines]
+  (drop 1 lines))
+
+(defn track-dup
+  [state idx line]
+  (let [columns (split-line line)
+        target  (nth columns idx)]
+    (if-not (empty? (get state target))
+      (update-in state [target] conj columns)
+      (assoc state target [columns]))))
+
+(defn count-dup-phone-numbers
+  [lines]
+  (let [phone-number-idx 1]
+    (reducers/reduce (fn [acc line]
+                       (track-dup acc phone-number-idx line))
+                     {}
+                     lines)))
+
+(comment
+  (let [lines (iota/seq "fixtures/001.tab")]
+    (->> lines
+         remove-header
+         count-dup-phone-numbers))
+)
