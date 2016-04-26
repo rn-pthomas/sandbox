ba622006b9c3e646c235a3c373b7d9060916e8b2
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-04-26 11:50:10 -0400

    new lein project

---
+++ file-validation/test/file_validation/core_test.clj
@@ -0,0 +1,7 @@
+(ns file-validation.core-test
+  (:require [clojure.test :refer :all]
+            [file-validation.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
