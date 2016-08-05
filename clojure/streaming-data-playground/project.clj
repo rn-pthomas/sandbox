c3b2f0c18b0646badc2fe4bd2523e1147eff3603
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-08-05 17:48:00 -0400

    Add spark dependency.

--- clojure/streaming-data-playground/project.clj
+++ clojure/streaming-data-playground/project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [yieldbot/flambo     "0.7.2"]])
+  :dependencies [[org.clojure/clojure              "1.8.0"]
+                 [yieldbot/flambo                  "0.7.2"]
+                 [org.apache.spark/spark-core_2.10 "1.5.0"]])
