da2e53cf8eaa413b56c7cc33d3ebe1465ec7983a
pat-thomas <patthomassoftware@gmail.com>
2017-07-14 10:18:51 -0400

    new lein project

---
+++ clj-event-streaming/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-event-streaming
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clj-event-streaming/test/clj_event_streaming/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-event-streaming.core-test
+  (:require [clojure.test :refer :all]
+            [clj-event-streaming.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
