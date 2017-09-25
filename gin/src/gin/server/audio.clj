38654f169ff6acdcbca1781f43064470bbcbe016
pat-thomas <patthomassoftware@gmail.com>
2017-09-25 16:09:41 -0400

    Fixes to make recording actually work.

--- gin/src/gin/server/audio.clj
+++ gin/src/gin/server/audio.clj
@@ -21,7 +21,7 @@
 
 (defn reverse-target-data-bytes
   [target-data-bytes-array-size target-data-bytes & [{:keys [bytes-per-sample]
-                                                      :or {bytes-per-sample 2}}]]
+                                                      :or   {bytes-per-sample 2}}]]
   ;;(spit "log.out" (vec target-data-bytes) :append true)
   (->> target-data-bytes
        (partition bytes-per-sample)
@@ -33,13 +33,11 @@
 (defn buffer-n-messages
   "Use blocking take to take n messages off of channel c, and once done, applies function f to that vec of messages."
   [ch n f]
-  (println (format "buffer-n-messages: waiting for %s messages" n))
   (loop [acc     []
          counter 0]
     (if (>= counter n)
       (f acc)
       (let [new-message (a/<!! ch)]
-        (println "buffer-n-messages: read message.")
         (recur (conj acc new-message) (inc counter))))))
 
 (defn get-mixers
@@ -106,44 +104,49 @@
 
 (defn sample-thread-handler
   "Reads from sample-chan, buffers samples, applies processing, stores in clip store."
-  [{:keys [sample-chan data-bytes target-line source-line byte-array-size sample-store sample-buffer-size clip-chan clip-store]
-    :or   {sample-buffer-size 1}}]
+  [{:keys [sample-chan data-bytes target-line source-line byte-array-size sample-store buffer-size clip-chan clip-store]
+    :or   {buffer-size 1}}]
   (println "sample-thread-handler started")
   (while true
     (buffer-n-messages
      sample-chan
-     sample-buffer-size
+     buffer-size
      (fn [messages]
-       (let [{:keys [all-bytes all-bytes-read]} (reduce (fn [acc {:keys [num-bytes-read audio-bytes-vec frame-size]}]
+       (let [{:keys [all-bytes all-bytes-read]} (reduce (fn [acc {:keys [num-bytes-read data-bytes-vec frame-size] :as sample}]
+                                                          (swap! sample-store conj sample)
                                                           (-> acc
-                                                              (update-in [:all-bytes] #(concat % audio-bytes-vec))
+                                                              (update-in [:all-bytes] #(concat % data-bytes-vec))
                                                               (update-in [:all-bytes-read] (partial + num-bytes-read))))
                                                         {:all-bytes      []
                                                          :all-bytes-read 0}
                                                         messages)
-             num-bytes-to-allocate              byte-array-size
+             all-bytes                          (vec all-bytes)
+             num-bytes-to-allocate              all-bytes-read
              non-processed-bytes                (byte-array num-bytes-to-allocate all-bytes)
              reversed-bytes                     (reverse-target-data-bytes num-bytes-to-allocate all-bytes)]
          (swap! clip-store conj {:forward        non-processed-bytes
                                  :reversed       reversed-bytes
                                  :all-bytes-read all-bytes-read})
          ;; TODO: figure out what clip to play and put on clip-chan. For now just randomly sample from last two elements of clip-store
-         (a/>!! clip-chan (rand-nth (take-last 2 @clip-store))))))))
+         (a/>!! clip-chan (rand-nth (take-last 4 @clip-store))))))))
 
 (defn playback-thread-handler
   [{:keys [sample-chan data-bytes target-line source-line byte-array-size sample-store sample-buffer-size clip-chan clip-store]
     :or   {sample-buffer-size 1}}]
   (println "playback-thread-handler started")
   (while true
-    (let [{:keys [forward reversed all-bytes-read]} (a/<!! clip-chan)]
-      ;;(println "forward:" forward)
-      ;;(println "reversed:" reversed)
-      ;;(println "all-bytes-read:" all-bytes-read)
-      (.write source-line forward  0 all-bytes-read)
+    (let [{:keys [forward reversed all-bytes-read] :as msg} (a/<!! clip-chan)]
+      (.write source-line forward 0 all-bytes-read)
+      (.write source-line reversed 0 all-bytes-read)
+      (.write source-line forward 0 all-bytes-read)
       (.write source-line reversed 0 all-bytes-read))))
 
+(comment
+  (stream! {:ms 20000 :init-ms 500, :input-type :mic :buffer-size 10})
+  )
+
 (defn stream!
-  [{:keys [ms init-ms input-type]}]
+  [{:keys [ms init-ms input-type buffer-size] :as params}]
   (let [audio-format        (create-audio-format {:sample-rate 44100 :channels 2})
         target-line         (input-type->target-line input-type audio-format)
         source-line         (input-type->source-line input-type audio-format)
@@ -163,7 +166,8 @@
                              :clip-chan       clip-chan
                              :sample-chan     sample-chan
                              :clip-store      clip-store
-                             :sample-store    sample-store}
+                             :sample-store    sample-store
+                             :buffer-size     buffer-size}
         recording-thread    (Thread. #(recording-thread-handler thread-params))
         sample->clip-thread (Thread. #(sample-thread-handler    thread-params))
         playback-thread     (Thread. #(playback-thread-handler  thread-params))]
@@ -177,16 +181,9 @@
     (cleanup {:threads [recording-thread sample->clip-thread playback-thread]
               :lines   [target-line source-line]})
     (println (format "Done. Captured %s clips" (count @clip-store)))
+    (println (format "Done. params = %s" params))
     :done))
 
 (defn seconds->ms
   [seconds]
   (* 1000 seconds))
-
-(comment
-  ;; this is how you start playback...
-  (stream! {:filename   "foo.wav"
-            :ms         (seconds->ms 30)
-            :init-ms    5000
-            :input-type "microphone"})
-)
