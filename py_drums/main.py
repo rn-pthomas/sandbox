9ab675001df59865414718c448817736dbc28eb8
pat-thomas <patthomassoftware@gmail.com>
2018-10-20 13:34:14 -0400

    sure

--- py_drums/main.py
+++ py_drums/main.py
@@ -2,6 +2,7 @@ import json
 import copy
 import random
 import sys
+import mido
 
 config = json.loads(open("config.json").read())
 instruments = config["instruments"]
@@ -53,6 +54,36 @@ def calc_convergence():
     all_repetitions = map(lambda k: instruments[k]["repetitions"], instruments.keys())
     return apply(lcm, all_repetitions)
 
+def midi_on(tick_to_use):
+    return mido.Message("note_on", note=60, time=tick_to_use)
+    #return midi.NoteOnEvent(tick=tick_to_use, velocity=0, pitch=midi.G_3)
+
+def midi_off(tick_to_use):
+    return mido.Message("note_off", note=60, time=tick_to_use)
+    #return midi.NoteOnEvent(tick=tick_to_use, velocity=0, pitch=midi.G_3)
+
+def to_midi(seq):
+    #pattern = midi.Pattern()
+    #track = midi.Track()
+    #pattern.append(track)
+    for idx, el in enumerate(seq):
+        for inst in el.keys():
+            inst_pattern = el[inst]
+            pattern_length = len(inst_pattern)
+            for measure_tick, note in enumerate(inst_pattern):
+                if (measure_tick == 0 or idx == 0):
+                    tick_to_use = idx
+                else:
+                    tick_to_use = idx + (measure_tick/float(idx))
+
+                print({"on": tick_to_use, "off": tick_to_use + 0.05})
+
+                if (note != 0):
+                    on = midi_on(tick_to_use)
+                    off = midi_off(tick_to_use)
+                    #track.append(on)
+                    #track.append(off)
+
 def main():
     inst = None
     if (len(sys.argv) > 1):
@@ -60,10 +91,12 @@ def main():
 
     convergence = calc_convergence()
     patterns = evolve(convergence)
+    the_midi = to_midi(patterns)
     if inst is not None:
         print json.dumps(map(lambda item: item[inst], patterns))
         return
-    print (json.dumps(patterns))
+    #print (json.dumps(patterns))
+    print (mido.get_output_names())
 
 if __name__ == "__main__":
     main()
